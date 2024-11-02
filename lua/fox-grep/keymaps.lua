d0ecf68298fc88400aa0e929f69976dfac02e6c7
Cannafox <gklodkowski@gmail.com>
2024-11-02 02:17:10 +0100

    test

--- lua/fox-grep/keymaps.lua
+++ lua/fox-grep/keymaps.lua
@@ -8,10 +8,14 @@ function M.setup(opts)
   opts = opts or {}
 
   if opts.verbose then
-    M.logger = require("fox-grep.utils.logger").init("lua/fox-grep/keymaps.lua")
+    M.logger = require("fox-grep.utils.logger")
+    M.logger.init("lua/fox-grep/keymaps.lua")
   end
 
   for _, keymap in ipairs(M.default) do
+    if opts.verbose then
+      M.logger.print(string.format("Setting keymap: %s", keymap.toString()))
+    end
     vim.keymap.set(unpack(keymap))
   end
 end
