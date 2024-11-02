44f9190ed266e84aabdec4ce87beeacfc6432747
Cannafox <gklodkowski@gmail.com>
2024-11-02 01:52:08 +0100

    test

--- lua/fox-grep.lua
+++ lua/fox-grep.lua
@@ -3,6 +3,7 @@ local M = {}
 ---@param opts table
 function M.setup(opts)
   local logger = require("fox-grep.utils.logger").init("lua/fox-grep.lua")
+  local core = require("fox-grep.grep_core")
 end
 
 return M
